3acbaf63e9a655ea5f992a16b3881c735ab896dd
hmilenko <hugo.milenkovic@gmail.com>
2022-08-07 20:32:07 +0000

    my first modif

--- metar_taf.py
+++ metar_taf.py
@@ -4,5 +4,11 @@ weather_code_list = weather_code.split(" ")
 
 print(weather_code_list)
 
+# this is a new line
+
+#this in another line
+
+print("Hello World!")
+
 # Si le premier item de la liste == "TAF" ou le 3e item est sous le format "xxxx/xxxx" alors le code est de class TAF
 # Sinon le code est de class METAR
