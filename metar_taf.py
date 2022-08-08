5988405bce3837dd0918d227c71c4f2cd8f3437e
hmilenko <hugo.milenkovic@gmail.com>
2022-08-08 17:19:44 +0000

    clean up

--- metar_taf.py
+++ metar_taf.py
@@ -1,12 +1,53 @@
+import re
+
+class Metar:
+    def __init__(self, data):
+        self.data = data
+
+    def terrain_id(self):
+        print(self.data[0])
+
+    def wind(self):
+        print(self.data[1])
+
+    def visibility(self):
+        print(self.data[2])
+
+    def temperatures(self):
+        print(self.data[3])
+
+    def pressure(self):
+        print(self.data[4])
+
+    def trend(self):
+        print(self.data[5])
+
+
+class Taf:
+    def __init__(self, data) -> None:
+        self.data = data
+
 weather_code = input("Entrez le message à décoder : ")
 
 weather_code_list = weather_code.split(" ")
 
-print(weather_code_list)
+# Indicators to determine if TAF, else is METAR
+taf_indicator_1 = weather_code_list[0]
+taf_indicator_2 = len(re.sub("[^0-9]", "", weather_code_list[2]))
 
-#this in another line
+# First occurence of time FROM-TO contains 8 digits
+if taf_indicator_1 == "TAF" or taf_indicator_2 == 8:
+    print("C'est un TAF")
+    weather_decode = Taf(weather_code_list)
+    print(weather_decode.data)
 
-print("Hello World!")
-
-# Si le premier item de la liste == "TAF" ou le 3e item est sous le format "xxxx/xxxx" alors le code est de class TAF
-# Sinon le code est de class METAR
+else:
+    print("C'est un METAR")
+    weather_decode = Metar(weather_code_list)
+    print(weather_decode.data)
+    weather_decode.terrain_id()
+    weather_decode.wind()
+    weather_decode.visibility()
+    weather_decode.temperatures()
+    weather_decode.pressure()
+    weather_decode.trend()
